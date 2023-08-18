2b59327d22ae0b62e1b16ac44a7608bb8537fe51
Natalya Belikova <minaevanv@inbox.ru>
2023-08-18 16:21:09 +0300

    docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.enableCors(corsOption);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
