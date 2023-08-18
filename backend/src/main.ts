c17f48aef07d87974ed053081fc07235b92e4f21
Natalya Belikova <minaevanv@inbox.ru>
2023-08-18 18:42:38 +0300

    server url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOption: CorsOptions = {
-    origin: 'http://localhost:8081',
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-    allowedHeaders: ['Content-type', 'Authorization'],
-  };
-  app.enableCors(corsOption);
+  // const corsOption: CorsOptions = {
+  //   origin: 'http://localhost:8081',
+  //   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  //   allowedHeaders: ['Content-type', 'Authorization'],
+  // };
+  // app.enableCors(corsOption);
   
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.SERVER_PORT || 3000);
