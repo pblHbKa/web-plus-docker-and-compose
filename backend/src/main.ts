0c8185d928259ad60f1aec82b91c4971c3a89176
Natalya Belikova <minaevanv@inbox.ru>
2023-08-18 20:35:39 +0300

    delete comments

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // const corsOption: CorsOptions = {
-  //   origin: 'https://pblhbkayandex.nomoreparties.co',
-  //   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-  //   allowedHeaders: ['Content-type', 'Authorization'],  
-  // };
   app.enableCors();
   
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
